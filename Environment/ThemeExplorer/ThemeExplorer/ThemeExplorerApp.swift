de31c41bf769771690e2662ae7ef29f7c8401d36
zhishanggarden <zhishanggarden@gmail.com>
2026-06-29 09:24:56 -0700

    feat(archive_001 (@environment)): initial commit

---
+++ Environment/ThemeExplorer/ThemeExplorer/ThemeExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeExplorerApp.swift
+//  ThemeExplorer
+//
+//  Created by D F on 6/29/26.
+//
+
+import SwiftUI
+
+@main
+struct ThemeExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
